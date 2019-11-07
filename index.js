750f550660cc3a78b3270292c581ba18f91d2e24
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-11-07 20:23:44 +0000

    initial

---
+++ index.js
@@ -0,0 +1,73 @@
+const ansi = require('ansi-escape-sequences')
+const DefaultView = require('@test-runner/default-view')
+
+class OnelineView extends DefaultView {
+  constructor (options) {
+    super(options)
+    this.firstLine = true
+    this.fails = []
+  }
+
+  start () {
+  }
+
+  log (...args) {
+    const msgSize = process.stdout.columns - 30
+    const msg = args.join(' ')
+    const stringLength = require('string-length')
+    if (!this.firstLine) {
+      process.stdout.write(ansi.cursor.up(1) + ansi.erase.inLine(2))
+    }
+    let toDisplay = msg.substr(0, msgSize)
+    let msgLen = stringLength(toDisplay)
+    let paddingRequired = msgSize - msgLen
+    toDisplay = msg.substr(0, msgSize + paddingRequired)
+    msgLen = stringLength(toDisplay)
+    paddingRequired = msgSize - msgLen
+
+    const stats = this.runner.stats
+    const failColour = stats.fail > 0 ? 'red' : 'white'
+    const passColour = stats.pass > 0 ? 'green' : 'white'
+    const skipColour = stats.skip > 0 ? 'grey' : 'white'
+    const s = ansi.format(`Pass: [${passColour}]{${stats.pass}}, fail: [${failColour}]{${stats.fail}}, skip: [${skipColour}]{${stats.skip}}.`)
+    console.log(s, toDisplay + ' '.repeat(paddingRequired), ansi.style.reset)
+    this.firstLine = false
+  }
+
+  testFail (test, err) {
+    const indent = ' '.repeat(test.level())
+    const parent = test.parent ? test.parent.name : ''
+    this.fails.push(ansi.format(`${indent}[red]{⨯} [magenta]{${parent}} ${test.name}`))
+    const lines = this.getErrorMessage(err).split('\n').map(line => {
+      const indent = ' '.repeat(test.level() + 2)
+      return indent + line
+    })
+    this.fails.push(ansi.format(`\n${lines.join('\n').trimEnd()}\n`))
+  }
+
+  getErrorMessage (err) {
+    if (this.options.viewHideErrStack) {
+      return err.message
+    } else {
+      return err.stack
+    }
+  }
+
+  /**
+   * @params {object} stats
+   * @params {object} stats.fail
+   * @params {object} stats.pass
+   * @params {object} stats.skip
+   * @params {object} stats.start
+   * @params {object} stats.end
+   */
+  end (stats) {
+    this.log(ansi.format(`Completed in ${stats.timeElapsed()}ms.`))
+    if (this.fails.length) {
+      console.log()
+      console.log(this.fails.join('\n'))
+    }
+  }
+}
+
+module.exports = OnelineView
